0b185cdb140b460f28d84da75e807ec8c80897cc
Igor Gnatenko <i.gnatenko.brain@gmail.com>
2016-09-01 09:12:27 +0200

    examples: add maxout example from util-linux
    Signed-off-by: Igor Gnatenko <i.gnatenko.brain@gmail.com>

---
+++ examples/maxout.py
@@ -0,0 +1,23 @@
+from __future__ import print_function, unicode_literals
+import locale
+
+import smartcols
+
+if __name__ == "__main__":
+    locale.setlocale(locale.LC_ALL, '')
+
+    tb = smartcols.Table()
+    tb.maxout = True
+
+    cl_left = tb.new_column("LEFT")
+    cl_foo = tb.new_column("FOO")
+    cl_right = tb.new_column("RIGHT")
+    cl_right.right = True
+
+    for _ in reversed(range(3)):
+        ln = tb.new_line()
+        ln[cl_left] = "A"
+        ln[cl_foo] = "B"
+        ln[cl_right] = "C"
+
+    print(tb)
